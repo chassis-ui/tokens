a4e4b7b09e39025134701e12b9f2901cab335a34
Ozgur Gunes <o.gunes@gmail.com>
2026-09-24 19:52:07 +0300

    release v0.5.1

--- build/tokens/build.js
+++ build/tokens/build.js
@@ -58,7 +58,7 @@ function registerDictionary() {
     name: 'cxFileHeader',
     fileHeader: async (defaultMessages = []) => [
       ...defaultMessages,
-      `Chassis - Tokens v0.5.0`,
+      `Chassis - Tokens v0.5.1`,
       `Copyright 2026 Ozgur Gunes`,
       `Licensed under MIT (https://github.com/chassis-ui/tokens/blob/main/LICENSE)`
     ]
